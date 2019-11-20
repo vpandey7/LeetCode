de59b8e4a983abea1dcfb04b42d13b85ebd5a46b
Pandey Vishal <vishal_pandey@outlook.com>
2019-11-20 20:18:18 +0530

    20 Valid Parentheses

---
+++ 20 - Valid Parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    var stack = [];
+    for (var c of s) {
+        if (c === '(' || c === '[' || c === '{') {
+            stack.push(c);
+        } else if (c === ')' && stack.length > 0 && stack[stack.length - 1] === '(') {
+            stack.pop();
+        } else if (c === ']' && stack.length > 0 && stack[stack.length - 1] === '[') {
+            stack.pop();
+        } else if (c === '}' && stack.length > 0 && stack[stack.length - 1] === '{') {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
