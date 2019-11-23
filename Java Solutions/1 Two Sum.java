46623251e76fcb50b73ad4b0f92f950295d09964
Pandey Vishal <vishal_pandey@outlook.com>
2019-11-23 22:14:56 +0530

    1 Two Sum

---
+++ Java Solutions/1 Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] result = new int[2];
+        for(int i =0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                result[0] = i;
+                result[1] = map.get(target-nums[i]);
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return result;
+        
+    }
+}
