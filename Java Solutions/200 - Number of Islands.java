fb91d669781088f538a9b8ec5368ea97be9e4774
Pandey Vishal <vishal_pandey@outlook.com>
2019-12-28 20:21:28 +0530

    Number of Islands

---
+++ Java Solutions/200 - Number of Islands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+
+        int numIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    callDFS(grid, i, j);
+                }
+            }
+        }
+        return numIslands;
+
+    }
+
+    public void callDFS(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        callDFS(grid, i + 1, j);
+        callDFS(grid, i - 1, j);
+        callDFS(grid, i, j + 1);
+        callDFS(grid, i, j - 1);
+    }
+}
