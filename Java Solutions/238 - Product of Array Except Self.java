b91746b36cbd39c6d94222cbe8aba74fc65048fd
Pandey Vishal <vishal_pandey@outlook.com>
2019-12-30 22:40:55 +0530

    Product of Array Except Self

---
+++ Java Solutions/238 - Product of Array Except Self.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int N = nums.length;
+
+        int[] l = new int[N];
+        int[] r = new int[N];
+
+        int[] result = new int[N];
+
+        l[0] = 1;
+
+        for (int i = 1; i < N; i++) {
+            l[i] = l[i - 1] * nums[i - 1];
+        }
+
+        r[N - 1] = 1;
+        for (int i = N - 2; i >= 0; i--) {
+            r[i] = r[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < N; i++) {
+            result[i] = l[i] * r[i];
+        }
+        return result;
+
+    }
+}
